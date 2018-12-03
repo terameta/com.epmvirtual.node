cd55f94c5b4ce541f6889d6989e5df834ec7d110
Ali Dikici <aliriza.dikici@gmail.com>
2018-12-03 23:41:16 +0400

    Regular Commit

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
 import { EPMNode } from "./server.application";
 
 const app = new EPMNode();
-
-console.log( process.env.NODE_PORT );
-console.log( process.env.Firebase_User );
-console.log( process.env.Firebase_Pass );
