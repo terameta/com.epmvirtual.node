12331c19d9493d01191c4a3933c0a57e1e41ec3f
Ali Dikici <aliriza.dikici@gmail.com>
2018-12-03 22:49:55 +0400

    Regular Commit

--- models/node.ts
+++ models/node.ts
@@ -1,62 +1,76 @@
-export interface Node extends NodeCandidate {
-	name: string,
-	terminal: {
-		requested: boolean,
-		dimensions: {
-			cols: number, rows: number
-		}
-	},
-	lastCommandResult: string,
-	lastCommand: string,
-	currentCommand: string,
-	poolAssignments: PoolAssignment,
-	poolWorkerAssignments: PoolWorkerAssignment
-}
-
-export const defaultNode = (): Node => ( { id: '', name: '', terminal: { dimensions: { cols: 0, rows: 0 } }, poolAssignments: {}, poolWorkerAssignments: {} } as Node );
-
-export interface NodeCandidateObject {
-	id: string,
-	items: NodeCandidate[]
-}
-
-export interface NodeCandidate {
+export interface Node {
 	id: string,
 	name: string,
-	details: any,
-	hostname: string,
-	ostype: string,
-	osplatform: string,
-	osarch: string,
-	osrelease: string,
-	keypresses: KeyPress[],
-	responses: PtyResponse[],
-	commands: NodeCommand[]
+	networkInterfaces: NodeNetworkInterfacesData[]
 }
 
-export interface KeyPress {
-	date: any,
-	key: string,
-	dateValue?: Date
+interface NodeNetworkInterfacesData {
+	iface: string;
+	ip4: string;
+	ip6: string;
+	mac: string;
+	internal: boolean;
 }
 
-export interface PtyResponse {
-	date: any,
-	datum: string,
-	dateValue?: Date
-}
+// export interface Node extends NodeCandidate {
+// 	name: string,
+// 	terminal: {
+// 		requested: boolean,
+// 		dimensions: {
+// 			cols: number, rows: number
+// 		}
+// 	},
+// 	lastCommandResult: string,
+// 	lastCommand: string,
+// 	currentCommand: string,
+// 	poolAssignments: PoolAssignment,
+// 	poolWorkerAssignments: PoolWorkerAssignment
+// }
 
-export interface NodeCommand {
-	date: any,
-	command: string,
-	dateValue?: Date
-}
+// export const defaultNode = (): Node => ( { id: '', name: '', terminal: { dimensions: { cols: 0, rows: 0 } }, poolAssignments: {}, poolWorkerAssignments: {} } as Node );
+
+// export interface NodeCandidateObject {
+// 	id: string,
+// 	items: NodeCandidate[]
+// }
+
+// export interface NodeCandidate {
+// 	id: string,
+// 	name: string,
+// 	details: any,
+// 	hostname: string,
+// 	ostype: string,
+// 	osplatform: string,
+// 	osarch: string,
+// 	osrelease: string,
+// 	keypresses: KeyPress[],
+// 	responses: PtyResponse[],
+// 	commands: NodeCommand[]
+// }
+
+// export interface KeyPress {
+// 	date: any,
+// 	key: string,
+// 	dateValue?: Date
+// }
+
+// export interface PtyResponse {
+// 	date: any,
+// 	datum: string,
+// 	dateValue?: Date
+// }
+
+// export interface NodeCommand {
+// 	date: any,
+// 	command: string,
+// 	dateValue?: Date
+// }
 
 
-export interface PoolAssignment {
-	[ key: string ]: boolean
-}
+// export interface PoolAssignment {
+// 	[ key: string ]: boolean
+// }
 
-export interface PoolWorkerAssignment {
-	[ key: string ]: boolean
-}
+// export interface PoolWorkerAssignment {
+// 	[ key: string ]: boolean
+// }
