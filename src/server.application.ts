4e982a505ba016efdc5e103c642c04adbdbca950
Ali Dikici <aliriza.dikici@gmail.com>
2018-12-03 22:10:37 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -13,7 +13,7 @@ export class EPMNode {
 		// si.cpu().then( console.log );
 		// si.blockDevices().then( console.log );
 		si.networkInterfaces().then( console.log );
-		si.networkConnections().then( console.log );
+		// si.networkConnections().then( console.log );
 	}
 }
 // import * as os from 'os';
