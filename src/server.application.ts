44f59e47b0dafd500a875fcdf72123959d809eac
Ali Dikici <aliriza.dikici@gmail.com>
2018-12-16 16:28:13 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -59,11 +59,40 @@ export class EPMNode {
 		console.log( 'Initiating RTC channel' );
 		const servers = { 'iceServers': [ { 'urls': 'stun:stun.l.google.com:19302' } ] };
 		const pc = new wrtc.RTCPeerConnection( servers );
-		Object.keys( pc ).forEach( k => console.log( k ) );
 		pc.onicecandidate = ( event: any ) => {
 			console.log( 'RTC On ice candidate' );
 			console.log( event );
 		}
+		pc.onsignalingstatechange = ( event ) => {
+			console.info( 'Signaling state change:', event.target.signalingState );
+		}
+		/**
+		 pc.onsignalingstatechange = function(event) {
+    console.info('signaling state change: ', event.target.signalingState);
+  };
+  pc.oniceconnectionstatechange = function(event) {
+    console.info('ice connection state change: ', event.target.iceConnectionState);
+  };
+  pc.onicegatheringstatechange = function(event) {
+    console.info('ice gathering state change: ', event.target.iceGatheringState);
+  };
+  pc.onicecandidate = function(event) {
+    var candidate = event.candidate;
+    if (!candidate) return;
+    if (WebSocket.OPEN === ws.readyState) {
+      ws.send(JSON.stringify({
+        type: 'ice',
+        sdp: {
+          candidate: candidate.candidate,
+          sdpMid: candidate.sdpMid,
+          sdpMLineIndex: candidate.sdpMLineIndex
+        }
+      }));
+    } else {
+      pendingCandidates.push(candidate);
+    }
+  };
+		 */
 	}
 
 	private identifySelf = async () => {
