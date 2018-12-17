298a557da65a205e432a0824eb6996611b76fb14
Ali Dikici <aliriza.dikici@gmail.com>
2018-12-17 23:34:43 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -147,6 +147,11 @@ export class EPMNode {
 		pc.onicecandidate = ( candidate ) => {
 			console.log( 'We have ice candidate', candidate );
 		}
+		pc.ondatachannel = ( event ) => {
+			console.log( 'Data channel is now open' );
+			console.log( event.channel );
+			console.log( event );
+		}
 		console.log( 'Peer connection is now created' );
 		await pc.setRemoteDescription( new wrtc.RTCSessionDescription( offer ) );
 		console.log( 'Set remote description successful' );
@@ -156,11 +161,7 @@ export class EPMNode {
 		console.log( 'Local description is now set' );
 		await this.nodeReference.update( { rtc: { answer: JSON.stringify( pc.localDescription ) } } );
 		console.log( 'Answer is now delivered to firestore' );
-		pc.ondatachannel = ( event ) => {
-			console.log( 'Data channel is now open' );
-			console.log( event.channel );
-			console.log( event );
-		}
+
 
 		// const servers = { 'iceServers': [ { 'urls': 'stun:stun.l.google.com:19302' } ] };
 		// const pc = new wrtc.RTCPeerConnection( servers, { optional: [] } );
