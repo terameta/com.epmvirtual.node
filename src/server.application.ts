16e2baa553136a375c6e965fb6b2a35f00dd528f
Ali Dikici <aliriza.dikici@gmail.com>
2018-12-17 18:17:44 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -20,7 +20,7 @@ export class EPMNode {
 	private isNodeReceived = false;
 
 	constructor() {
-		interval( 60000 ).subscribe( () => console.log( 'EPMVirtual is reporting date:', new Date() ) );
+		interval( 120000 ).subscribe( () => console.log( 'EPMVirtual is reporting date:', new Date() ) );
 
 		this.initiate().catch( e => {
 			console.log( '!!! There is an issue with the initialization' );
