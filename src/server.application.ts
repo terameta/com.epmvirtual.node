8a6707645e74d9c015f3c62a6b2d529ddcb727b5
Ali Dikici <aliriza.dikici@gmail.com>
2018-12-17 22:42:45 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -152,6 +152,7 @@ export class EPMNode {
 		await pc.setLocalDescription( answer );
 		console.log( 'Local description is now set' );
 		await this.nodeReference.update( { rtc: { answer: JSON.stringify( pc.localDescription ) } } );
+		console.log( 'Answer is now delivered to firestore' );
 
 		// const servers = { 'iceServers': [ { 'urls': 'stun:stun.l.google.com:19302' } ] };
 		// const pc = new wrtc.RTCPeerConnection( servers, { optional: [] } );
