d8b966f7ca944aca31ed707d796d422e2244484b
Ali Dikici <aliriza.dikici@gmail.com>
2019-01-13 21:34:17 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -162,7 +162,9 @@ export class EPMNode {
 	private handlePoolsAction = async ( poolsSnapshot: firebase.firestore.QuerySnapshot ) => {
 		console.log( this.node.poolAssignments );
 		console.log( this.node.poolWorkerAssignments );
-		poolsSnapshot.docs.forEach( d => console.log( d.id, d.data() ) );
+		poolsSnapshot.docs.
+			map( d => ( <StoragePool>{ id: d.id, ...d.data() } ) ).
+			forEach( d => console.log( d ) );
 	}
 	private cancelPools = async () => { if ( this.poolsSubscription ) { this.poolsSubscription.unsubscribe(); this.poolsSubscription = null; } }
 
