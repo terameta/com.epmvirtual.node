eb52dc18f38862ea54a13347f35ee35124e2c92f
Ali Dikici <aliriza.dikici@gmail.com>
2019-01-13 21:29:22 +0400

    Regular Commit

---
+++ models/storagepool.models.ts
@@ -0,0 +1,29 @@
+export interface StoragePool {
+	id: string,
+	name: string,
+	rbdname: string,
+	dc: string,
+	monitors: string,
+	user: string,
+	secretuuid: string,
+	key: string,
+	files: StoragePoolFile[]
+}
+
+export interface StoragePoolFile {
+	name: string,
+	size: number,
+	actualsize: number
+}
+
+export const defaultStoragePool = (): StoragePool => ( {
+	id: '',
+	name: '',
+	rbdname: '',
+	dc: '',
+	monitors: '',
+	user: '',
+	secretuuid: '',
+	key: '',
+	files: []
+} );
