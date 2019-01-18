110f21d1864c8aad283a72d3f059495fdbf91bd8
Ali Dikici <aliriza.dikici@gmail.com>
2019-01-18 23:24:44 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -217,6 +217,9 @@ export class EPMNode {
 				console.log( 'We will now create the pool' );
 				( pool as any ).source = pool.monitors.split( ',' ).map( s => s.trim() ).map( s => { const [ address, port ] = s.split( ':' ).map( t => t.trim() ); return { address, port }; } );
 				const poolXML = await promisers.xmlCompile( pool, join( __dirname, './virsh/templates/pool.define.xml' ) );
+				const poolPath = '/tmp/' + pool.id + '.xml';
+				await promisers.writeFile( poolPath, poolXML );
+				await this.executeCommandAction( 'virsh pool-define --file ' + poolPath );
 				console.log( poolXML );
 				console.log( '===========================================' );
 				console.log( pool );
