4150f825694633e11b72b7f7a70a7203e5cfa225
Ali Dikici <aliriza.dikici@gmail.com>
2019-01-21 22:10:01 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -219,7 +219,7 @@ export class EPMNode {
 
 	private actAsPoolWorker = async ( payload: { pool: StoragePool, worker: boolean, timer: NodeJS.Timeout } ) => {
 		console.log( 'We are at actAsPoolWorker' );
-		const volumes = await returner( await this.executeCommandAction( 'virsh vol-list --pool ' + payload.pool.id ) );
+		const volumes = await returner( await this.executeCommandAction( 'virsh vol-list --details --pool ' + payload.pool.id ) );
 		console.table( volumes );
 	}
 
