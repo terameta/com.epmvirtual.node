53969634de0555227e5fd67dc2776500e89e0d08
Ali Dikici <aliriza.dikici@gmail.com>
2019-01-21 22:07:47 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -220,7 +220,7 @@ export class EPMNode {
 	private actAsPoolWorker = async ( payload: { pool: StoragePool, worker: boolean, timer: NodeJS.Timeout } ) => {
 		console.log( 'We are at actAsPoolWorker' );
 		const volumes = await returner( await this.executeCommandAction( 'virsh vol-list --pool ' + payload.pool.id ) );
-		console.log( volumes );
+		console.table( volumes );
 	}
 
 	private cancelPools = async () => { if ( this.poolsSubscription ) { this.poolsSubscription.unsubscribe(); this.poolsSubscription = null; } }
