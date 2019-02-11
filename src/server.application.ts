8f2ca6b63be4fde448bdc1db3bcba49f4117c769
Ali Dikici <aliriza.dikici@gmail.com>
2019-02-11 21:32:04 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -253,7 +253,7 @@ export class EPMNode {
 		for ( const volume of ( volArray as any[] ) ) {
 			const file = files[ volume.id ] || ( {} as StoragePoolFile );
 			if ( !file.lastCheck ) file.lastCheck = new Date();
-			console.log( file.id, file.lastCheck );
+			console.log( file.id, file.lastCheck, ( new Date() ).setDate( file.lastCheck.getDate() - 365 ) );
 
 		}
 		console.log( 'Number of registered files:', Object.keys( payload.pool.files ).length, '#WorkerRegistrations:', this.numberofWorkerRegistrations, 'FilesArti:', filesArti, 'FilesEksi:', filesEksi );
