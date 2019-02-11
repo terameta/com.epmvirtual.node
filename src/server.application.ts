6f778534f4401e3b489febc3d9a5c3b05117d04c
Ali Dikici <aliriza.dikici@gmail.com>
2019-02-11 22:44:42 +0400

    Regular Commit

--- src/server.application.ts
+++ src/server.application.ts
@@ -254,6 +254,7 @@ export class EPMNode {
 			sort( SortById ).
 			filter( v => !!files[ v.id ] ).
 			map( v => files[ v.id ] ).
+			map( v => { console.log( v.lastCheck ); return v; } ).
 			map( v => { v.lastCheck = v.lastCheck ? ( v.lastCheck as any ).toDate() : addDays( new Date(), -365 ); return v; } ).
 			filter( v => v.lastCheck < addDays( new Date(), -7 ) ).
 			filter( ( v, vi ) => ( vi < 1 ) ) ) {
